732c36d06f9c9a1c71f23c3b1272a54a43a915d9
Mystasly <mystasly48@gmail.com>
2015-06-11 23:56:58 +0900

    Update

--- RandomCodeGenerator/Form1.Designer.cs
+++ RandomCodeGenerator/Form1.Designer.cs
@@ -29,11 +29,132 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(12, 68);
+            this.textBox1.MaxLength = 100000;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(360, 77);
+            this.textBox1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.textBox1, "生成された文字列が表示されます");
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDown1.Location = new System.Drawing.Point(121, 22);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(134, 31);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.toolTip1.SetToolTip(this.numericUpDown1, "生成させる文字列の文字数を指定してください");
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(121, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "生成";
+            this.toolTip1.SetToolTip(this.button1, "指定された文字数で文字列を生成させます");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(0, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "文字列をコピー";
+            this.toolTip1.SetToolTip(this.button2, "生成された文字列をクリップボードにコピーします");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 214);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(384, 23);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            this.toolTip1.SetToolTip(this.statusStrip1, "現在のステータスが表示されます");
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(56, 18);
+            this.toolStripStatusLabel1.Text = "待機中...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 237);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Random Code Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- RandomCodeGenerator/Form1.cs
+++ RandomCodeGenerator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace RandomCodeGenerator
 {
@@ -15,6 +16,73 @@ namespace RandomCodeGenerator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string returnValue;
+        int numericValue;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Focus();
+        }
+
+        // Generate Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            toolStripStatusLabel1.Text = "文字列の生成中です...";
+            numericValue = (int)numericUpDown1.Value;
+            textBox1.Text = generateCode(numericValue);
+            toolStripStatusLabel1.Text = "文字列を生成しました！";
+            button1.Enabled = true;
+            button1.Focus();
+        }
+
+        // Copy the code Button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            toolStripStatusLabel1.Text = "文字列をコピー中です...";
+            copyClipboard(textBox1);
+            toolStripStatusLabel1.Text = "文字列をコピーしました！";
+            button2.Enabled = true;
+            button1.Focus();
+        }
+
+        private string generateCode(int length)
+        {
+            string codeChar = "0123456789abcdefghijklmnopqrstuvwxyz";
+            StringBuilder strBuild = new StringBuilder(length);
+            Random rand = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                int pos = rand.Next(codeChar.Length);
+                char code = codeChar[pos];
+                strBuild.Append(code);
+            }
+                return strBuild.ToString();
+        }
+
+        private void copyClipboard(TextBox box)
+        {
+            box.SelectAll();
+            box.Copy();
+        }
+
+        private void takeConfirm(String message)
+        {
+            DialogResult result = MessageBox.Show(message + Environment.NewLine + "続行しますか？", "警告", 
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show("続行します。");
+                returnValue = "continue";
+            }
+            else
+            {
+                MessageBox.Show("中止します。");
+                returnValue = "stop";
+            }
         }
     }
 }
